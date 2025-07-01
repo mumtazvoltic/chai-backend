98a03cbcb5116c8bed646b2c729bab85ba85e74d
Mumtaz Hussain <mumtaz@volticinc.dev>
2025-07-01 17:58:32 +0500

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    
+  })
+})
+.catch((err) => {
+    console.log("MONGO DB coneect failed !!!", err);
+    
+})
 
 // import express from "express";
 // const app = express();
