85340abae1b57c592a1eb80bf6313802fac71e9a
Mumtaz Hussain <mumtaz@volticinc.dev>
2025-07-08 11:51:34 +0500

    all setup configure

--- src/app.js
+++ src/app.js
@@ -8,8 +8,18 @@ app.use(cors({
     credentials: true
 }))
 app.use(express.json({limit: "16kb"}))
-app,use(express.urlencoded({extended: true,limit: "16kb"}))
+app.use(express.urlencoded({extended: true,limit: "16kb"}))
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+
+//routes 
+
+import userRouter from "./routes/user.routes.js";
+
+// route declerations 
+
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 export { app} 
